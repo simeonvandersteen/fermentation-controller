438dceefe5cc7d7c3b4e9724a61c75495006bcd5
Simeon van der Steen <simeonvandersteen@gmail.com>
2020-11-05 20:15:08 +0100

    Revert "write to display in one go to avoid out of sync glitches"
    This reverts commit 635ce45df5afc76abc70a801a71128026a6c939c.

--- test/test_display.py
+++ test/test_display.py
@@ -1,6 +1,7 @@
-from unittest.mock import PropertyMock, patch, call
+from time import sleep
+from threading import Thread
 
-import pytest
+from unittest.mock import Mock, PropertyMock, patch, call
 
 from fermentation_controller.display import Display
 
@@ -8,20 +9,13 @@ from fermentation_controller.display import Display
 class TestDisplay:
 
     @patch('fermentation_controller.display.CharLCD')
-    def test_lcd_inits_with_zero_temperature_and_switches_off(self, lcd_mock_class):
+    def test_clears_display_on_startup(self, lcd_mock_class):
         lcd_mock = lcd_mock_class.return_value
 
-        cursor_mock = PropertyMock()
-        type(lcd_mock).cursor_pos = cursor_mock
-
-        display = Display(["environment", "vessel", "fridge", "target"], ["heater", "cooler"])
-
-        display.run()
+        Display([], [])
 
         lcd_mock.clear.assert_called()
 
-        lcd_mock.write_string.assert_has_calls(
-            [call("E 0.0"), call("V 0.0"), call("F 0.0"), call("T 0.0"), call(" "), call(" ")])
 
     @patch('fermentation_controller.display.CharLCD')
     def test_writes_temperatures_to_lcd(self, lcd_mock_class):
@@ -33,21 +27,23 @@ class TestDisplay:
         display = Display(["environment", "vessel", "fridge", "target"], [])
 
         display.handle_temperature("environment", 12.3)
+        cursor_mock.assert_called_with((0, 0))
+        lcd_mock.write_string.assert_called_with("E 12.3")
+
         display.handle_temperature("vessel", 14.3)
+        cursor_mock.assert_called_with((0, 7))
+        lcd_mock.write_string.assert_called_with("V 14.3")
+
         display.handle_temperature("fridge", 11.3)
+        cursor_mock.assert_called_with((1, 0))
+        lcd_mock.write_string.assert_called_with("F 11.3")
+
         display.handle_temperature("target", 22.3)
+        cursor_mock.assert_called_with((1, 7))
+        lcd_mock.write_string.assert_called_with("T 22.3")
 
-        cursor_mock.assert_not_called()
-        lcd_mock.write_string.assert_not_called()
-
-        display.run()
-
-        cursor_mock.assert_has_calls([call((0, 0)), call((0, 7)), call((1, 0)), call((1, 7))])
-        lcd_mock.write_string.assert_has_calls([call("E 12.3"), call("V 14.3"), call("F 11.3"), call("T 22.3")])
-
-    @pytest.mark.parametrize("switch_status", [True, False, ])
     @patch('fermentation_controller.display.CharLCD')
-    def test_writes_switches_on_to_lcd(self, lcd_mock_class, switch_status):
+    def test_writes_switches_to_lcd(self, lcd_mock_class):
         lcd_mock = lcd_mock_class.return_value
 
         cursor_mock = PropertyMock()
@@ -55,14 +51,67 @@ class TestDisplay:
 
         display = Display([], ["heater", "cooler"])
 
-        display.handle_switch("heater", switch_status)
-        display.handle_switch("cooler", switch_status)
+        display.handle_switch("heater", True)
+        cursor_mock.assert_called_with((0, 14))
+        lcd_mock.write_string.assert_called_with("H")
+
+        display.handle_switch("heater", False)
+        cursor_mock.assert_called_with((0, 14))
+        lcd_mock.write_string.assert_called_with(" ")
+
+        display.handle_switch("cooler", True)
+        cursor_mock.assert_called_with((1, 14))
+        lcd_mock.write_string.assert_called_with("C")
+
+        display.handle_switch("cooler", False)
+        cursor_mock.assert_called_with((1, 14))
+        lcd_mock.write_string.assert_called_with(" ")
+
+    @patch('fermentation_controller.display.CharLCD')
+    def test_does_not_write_unknown_devices(self, lcd_mock_class):
+        lcd_mock = lcd_mock_class.return_value
+
+        cursor_mock = PropertyMock()
+        type(lcd_mock).cursor_pos = cursor_mock
+
+        display = Display(["vessel"], ["heater", "cooler"])
+
+        display.handle_switch("inbetweener", True)
+        display.handle_temperature("tropics", 42.3)
 
         cursor_mock.assert_not_called()
         lcd_mock.write_string.assert_not_called()
 
-        display.run()
+    @patch('fermentation_controller.display.CharLCD')
+    def test_writing_is_threadsafe(self, lcd_mock_class):
+        lcd_mock = lcd_mock_class.return_value
 
-        cursor_mock.assert_has_calls([call((0, 14)), call((1, 14))])
-        lcd_mock.write_string.assert_has_calls(
-            [call("H" if switch_status else " "), call("C" if switch_status else " ")])
+        cursor_mock = PropertyMock()
+        cursor_mock.side_effect = self.__delay
+        type(lcd_mock).cursor_pos = cursor_mock
+
+        display = Display(["environment"], ["heater"])
+
+        manager = Mock()
+        manager.attach_mock(lcd_mock, 'lcd_mock')
+        manager.cursor_mock = cursor_mock
+
+        t1 = Thread(target=display.handle_temperature, args=("environment", 12.3,))
+        t2 = Thread(target=display.handle_switch, args=("heater", True,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
+        calls_in_order = [
+            call.cursor_mock((0, 0)),
+            call.lcd_mock.write_string("E 12.3"),
+            call.cursor_mock((0, 14)),
+            call.lcd_mock.write_string("H")
+        ]
+
+        assert manager.mock_calls == calls_in_order
+
+    @staticmethod
+    def __delay(_: tuple) -> None:
+        sleep(0.01)
